3aa3c827c1ccf1238c81d12f3692ef3515376919
natalianeuhaus <natalia@casaneuhaus.com>
2026-09-04 10:17:36 -0400

    Add second Carpe Diem paragraph

--- assets/site.js
+++ assets/site.js
@@ -706,6 +706,7 @@
         targetId: "chapter-3",
         paragraphs: [
           "This is my being silly because why not, life is a bit of a lorem ipsum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
         ],
       },
     ];
